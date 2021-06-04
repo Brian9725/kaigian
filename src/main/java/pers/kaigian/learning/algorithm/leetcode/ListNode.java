c6c585453777fa1c0bdbaa628969e4e8bbfe93e1
hukaiyang <hukaiyang@huice.com>
2021-06-04 14:05:13 +0800

    优化leetcode结构

---
+++ src/main/java/pers/kaigian/learning/algorithm/leetcode/ListNode.java
@@ -0,0 +1,15 @@
+package pers.kaigian.learning.algorithm.leetcode;
+
+/**
+ * @author hukaiyang
+ * @date 2021-06-04 14:03
+ **/
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
