0d1283a05d8d2b312ab2e42c3ca8266653bd7ab0
BrianHu <1786104060@qq.com>
2021-11-21 19:29:49 +0800

    编写自定义栈

--- src/main/java/pers/kaigian/learning/algorithm/MyStack.java
+++ src/main/java/pers/kaigian/learning/algorithm/MyStack.java
@@ -4,5 +4,71 @@ package pers.kaigian.learning.algorithm;
  * @Author BrianHu
  * @Create 2021-04-25 10:40
  **/
-public class MyStack {
+public class MyStack<T> {
+
+    class Node {
+        T val;
+        Node next;
+
+        Node() {
+        }
+    }
+
+    private Node base;
+
+    private Node top;
+
+    private int size;
+
+    public MyStack() {
+        base = new Node();
+        top = base;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return (top == base);
+    }
+
+    public void push(T val) {
+        Node node = new Node();
+        node.val = val;
+        node.next = top;
+        top = node;
+        size++;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        Node ans = top;
+        top = top.next;
+        size--;
+        return ans.val;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        return top.val;
+    }
+
+    public static void main(String[] args) {
+        MyStack<Integer> integerMyStack = new MyStack<>();
+        System.out.println(integerMyStack.getSize());
+        System.out.println(integerMyStack.isEmpty());
+        System.out.println(integerMyStack.peek());
+        for (int i = 0; i < 5; i++) {
+            integerMyStack.push(i);
+        }
+        System.out.println(integerMyStack.pop());
+        System.out.println(integerMyStack.pop());
+        System.out.println(integerMyStack.getSize());
+    }
 }
